02b10c3548798ae837e330c3860c78b9a48d53a3
Zachary Lauzon <zachary.lauzon@bryx.com>
2014-04-05 17:39:55 -0400

    Added the actual GUI part

--- GUI.java
+++ GUI.java
@@ -1,26 +1,56 @@
 
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.Math;
+import javax.swing.*;
 
 class GUI extends JFrame {
 
-	public static final String[] CMDS = {
-		"python spiro.py"
+	// All commands the user can see
+	// First string is user-visible title, second is shell command
+	public static final String[][] CMDS = {
+		{"Spirograph", "python spiro.py"},
+		{"Recursive tree", "python drawTree.py"},
+		{"Recursive square", "python squareRec.py"},
+		{"Recursive I-shaped drawing", "python iTree.py"}
 	};
 
+	// Initialize the frame
 	public GUI () {
 		setTitle("Ut Totem - Computer Science project");
-		setSize(300,200); // default size is 0,0
-		setLocation(10,200); // default is 0,0 (top left corner)
+		setSize(600, 600);
+		setLocation(10,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addButtons();
 	}
 
 	public void addButtons() {
-		for (String cmd: CMDS) {
-			System.out.println(executeCommand(cmd));
+		// Calculate how man row / columns we need
+		int dim = (int) (Math.sqrt(CMDS.length) + 0.5);
+		// Set layout
+		setLayout(new GridLayout(dim, dim));
+
+		JButton btn;
+		for (final String[] cmd: CMDS) {
+			// Initialize button with user-visible name
+			btn = new JButton(cmd[0]);
+
+			// Register the button's on click listener (the command to exec)
+			btn.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					executeCommand(cmd[1]);
+				}
+			});
+
+			// Add this button to the layout
+			add(btn);
 		}
 	}
 
+	// Executes the shell command given
 	private String executeCommand(String command) {
  
 		StringBuffer output = new StringBuffer();
@@ -28,11 +58,11 @@ class GUI extends JFrame {
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
+			//p.waitFor();
 			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+                new BufferedReader(new InputStreamReader(p.getInputStream()));
  
-                        String line = "";			
+            String line = "";			
 			while ((line = reader.readLine())!= null) {
 				output.append(line + "\n");
 			}
@@ -45,10 +75,8 @@ class GUI extends JFrame {
  
 	}
 
+	// Sets up GUI and shows it to the user
 	public static void main(String[] args) {
-		GUI f = new GUI();
-		f.addButtons();
-		f.show();
-		
+		(new GUI()).show();
 	}
 }
