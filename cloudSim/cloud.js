44e6ed2a18f07204761d95041d167aa606d85472
jacksonhenry3 <jackson.henry3@gmail.com>
2014-04-05 13:28:07 -0400

    increased efficiency
    Increased efficiency by giving rays that are actively descending a
    separate class so that it doesn’t check collisions on those that have
    already gone through the cloud.

--- cloudSim/cloud.js
+++ cloudSim/cloud.js
@@ -3,11 +3,10 @@
 
 General notes
 ======================
-1. consider giving the lines that havent gotten stuck to the bottom a seperate class to improve effeciency
-2. Looking in to using d3.svg.line to do something similar to polyline to allow refraction
-3. change the data to be in polar coordinates for easy calculation (create conversion function)
+1. Looking in to using d3.svg.line to do something similar to polyline to allow refraction
+2. change the data to be in polar coordinates for easy calculation (create conversion function)
 
-4. different colors later
+3. different colors later
 
 */
 
@@ -18,8 +17,10 @@ var cloudParticleRadius = 6,
 	svgHeight           = Math.max(500, innerHeight)-50,
 	svgWidth     	    = Math.max(960, innerWidth)-50,	
 	iterTime			= 10,//length of an iteration cycle in ms
-	raySpeed			= 2,//ray speed in px per iteration
+	rayWidth            = 3,
+	raySpeed			= 8,//ray speed in px per iteration
 	rayData             = [],//data for each lightray
+	tau                 = 2*Math.PI,//used for conversion and polar coordinates
 	svg                 = d3.select("body").append("svg")// add the svg element to body
 							.attr("width" , svgWidth)
 							.attr("height", svgHeight)// send down a light ray on click
@@ -50,16 +51,17 @@ function addLine() {
 	var m = d3.mouse(this);
 
 	// adds new ray info to data array
-	rayData.push({x1:m[0],y1:m[1],x2:m[0],y2:m[1]})
-	svg.selectAll("line")
+	rayData.push({x1:m[0],y1:m[1],x2:m[0],y2:m[1],class:'activeRay'})
+	svg.selectAll(".activeRay")
 		.data(rayData)// associate all lines with a value in the rayData array
 		.enter()// d3 magic
 		.append("line")
 		.attr("x1",function(d){return(d.x1)})
 		.attr("y1",function(d){return(d.y1)}) 
 		.attr("x2",function(d){return(d.x2)})
-		 .attr("y2",function(d){return(d.y2+10)}) 
-		.attr("stroke-width",6)
+		.attr("y2",function(d){return(d.y2+10)}) 
+		.attr("class",function(d){return(d.class)})
+		.attr("stroke-width",rayWidth)
 		.attr("stroke","white");
 }
 
@@ -90,15 +92,53 @@ function replot()
 		if (rayY>svgHeight)
 		{
 		  rayData[i].y1=svgHeight-25
+		  rayData[i].class = 'inactive'
+		}
+
+		// change the class data so it doesnt keep being looped over after it hits the bottom
+		if (rayData[i].class == 'inactive')
+		{
+			rayData.splice(i,1)
 		}
 	}
 
 	// update svg with new data
-	svg.selectAll("line")
+	svg.selectAll(".activeRay")
 		.attr("x1",function(d){return(d.x1)})
 		.attr("x2",function(d){return(d.x1)})
 		.attr("y1",function(d){return(d.y1)})
-		.attr("y2",function(d){return(d.y2)});
+		.attr("y2",function(d){return(d.y2)})
+		.attr("class",function(d){return(d.class)});//this actualy changes the class
 };
+
+
+function polToCart(coordinates)
+{
+	r              = coordinates.r
+	theta          = coordinates.theta
+	x              = r*Math.cos(theta)
+	y              = r*Math.sin(theta)
+	newCoordinates = {x:x,y:y}
+	return(newCoordinates)
+}
+
+function cartToPol(coordinates)
+{
+	x              = coordinates.x
+	y              = coordinates.y
+	r              = Math.sqrt(Math.pow(x,2)+Math.pow(y,2))
+	theta          = Math.atan(y/x)
+	newCoordinates = {r:r,theta:theta}
+	return(newCoordinates)
+}
+
+var xScale = d3.scale.linear()
+	.domain([-svgWidth/2,svgWidth/2])
+	.range([0,svgWidth])
+
+var yScale = d3.scale.linear()
+	.domain([-svgHeight/2,svgHeight/2])
+	.range([svgHeight,0])
+
 
 window.setInterval(replot,iterTime)
