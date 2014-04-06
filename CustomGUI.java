423cdf07e51bf6cd2f227dafee5c03b4404a6f3f
ZACHARY RICHARD LAUZON <zrl3031@cs.rit.edu>
2014-04-06 16:10:26 -0400

    The write-your-own-recursion actually works! It looks horrid but I'll fix that

--- CustomGUI.java
+++ CustomGUI.java
@@ -1,43 +1,139 @@
-
-import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.Math;
 import javax.swing.BoxLayout;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-class CustomGUI extends JFrame {
+class CustomGUI extends JFrame implements ActionListener {
 
-	public static String cmd = "forward(length)\nleft(length)\nforward(length)\ndrawTree(length / 2, depth - 1)\nback(length)\nright(length)\nback(length)\n";
+	public static String cmd = "python3 customRec.py";
+	public static String length = "100";
+	public static String depth = "5";
+
+	public static String fd = "forward(length)";
+	public static String fdHalf = "forward(length/2)";
+	public static String bk = "backward(length)";
+	public static String rec = "drawTree(length/2,depth-1)";
+	public static String rt = "right(90)";
+	public static String lt = "left(90)";
+	public static String args;
+
+	public static final String[] CMDS = {
+		"move forward",
+		"move forward half",
+		"move backward",
+		"recurse",
+		"turn right",
+		"turn left"
+	};
+
+	public static final String[] VALS = {
+		fd,
+		fdHalf,
+		bk,
+		rec,
+		rt,
+		lt
+	};
+
+	public static final ArrayList<String> selectedCommands = new ArrayList<String>();
+	public static JList commandList;
+	public static JList selectedCommandsList;
 
 	// Initialize the frame
 	public CustomGUI () {
 		setTitle("Ut Totem - Computer Science project");
 		setSize(600, 600);
-		setLocation(10,200);
+		setLocation(10,10);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		addButtons(this.getContentPane());
+		
+		// Add default commands
+		selectedCommands.add(fd);
+		selectedCommands.add(lt);
+		selectedCommands.add(fd);
+		selectedCommands.add(rec);
+		selectedCommands.add(bk);
+		selectedCommands.add(rt);
+		selectedCommands.add(bk);
+
+		// Add elements to content pane
+		addElements(this.getContentPane());
+
 	}
 
-	public void addButtons(Container pane) {
+	public void addElements(Container pane) {
+
 
 		// Set layout
 		setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
-		JButton btn;
-		btn = new JButton("Run program");
+		// This button is used to add commands
+		final JButton addButton = new JButton("Add");
+		addButton.setActionCommand("Add");
+		addButton.addActionListener(this);
+
+		// This button is used to clear commands
+		final JButton clearButton = new JButton("Clear");
+		clearButton.setActionCommand("Clear");
+		clearButton.addActionListener(this);
+
+		// Add list of commands to layout
+		commandList = new JList(CMDS);
+		commandList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		commandList.setLayoutOrientation(JList.VERTICAL);
+		commandList.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					addButton.doClick(); // emulate button
+				}
+			}
+		});
+
+		// Add list of selected commands to layout
+		selectedCommandsList = new JList(selectedCommands.toArray());
+
+
+		// Add command button
+		JButton btn = new JButton("Run program");
 
 		// Register the button's on click listener (the command to exec)
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				executeCommand("python3 customRec.py 100 3 \"" + cmd + "\"");
+				args = "";
+				for (String cmd: selectedCommands) {
+					args += cmd + " ";
+				}
+				executeCommand(cmd + " " + length + " " + depth + " " + args);
 			}
 		});
 
-		// Add this button to the layout
+		// Add buttons to the layout
+		pane.add(commandList);
+		pane.add(addButton);
+		pane.add(clearButton);
+		pane.add(selectedCommandsList);
 		pane.add(btn);
+	}
+
+	// Handle click events for add button
+	public void actionPerformed(ActionEvent e) {
+		if ("Add".equals(e.getActionCommand())) {
+			String val = (String) commandList.getSelectedValue();
+			if (val != null) {
+				selectedCommands.add(VALS[Arrays.asList(CMDS).indexOf(val)]);
+			}
+
+			// Update selected commands
+			selectedCommandsList.setListData(selectedCommands.toArray());
+		}
+		else if ("Clear".equals(e.getActionCommand())) {
+			selectedCommands.clear();
+			selectedCommandsList.setListData(selectedCommands.toArray());
+		}
 	}
 
 	// Executes the shell command given
@@ -48,7 +144,7 @@ class CustomGUI extends JFrame {
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
-			//p.waitFor();
+			p.waitFor();
 			BufferedReader reader = 
                 new BufferedReader(new InputStreamReader(p.getInputStream()));
  
