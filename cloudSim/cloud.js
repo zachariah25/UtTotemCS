b9aae8c6cb48adba76cc2c8a8cd5be7e48bc2510
jacksonhenry3 <jackson.henry3@gmail.com>
2014-04-19 14:40:33 -0400

    updated asthetic

--- cloudSim/cloud.js
+++ cloudSim/cloud.js
@@ -5,6 +5,7 @@ General notes
 =====================
 1. reset origin at more accurate location
 2. increase efficiency
+3.
 
 ????????????????????
 
@@ -14,28 +15,34 @@ General notes
 
 */
 
-var cloudParticleRadius = 4,
+var cloudParticleRadius = 10,
 	numCloudParticles   = 500,
 	cloudParticles      = [],
 	cloudColor          = "rgba(255,255,255,.4)",
-	svgHeight           = Math.max(500, innerHeight)-50,
-	svgWidth     	    = Math.max(960, innerWidth)-50,	
+	svgHeight           = Math.max(500, innerHeight)-5,
+	svgWidth     	    = Math.max(960, innerWidth)-20,	
 	iterTime			= 10,//length of an iteration cycle in ms
-	rayWidth            = 2,
-	rayColor            = "#FFCC00",
-	raySpeed			= 5,//ray speed in px per iteration
+	rayWidth            = 7,
+	rayColor            = "rgba(255,255,255,.5)",
+	raySpeed			= 7,//ray speed in px per iteration
 	rayData             = [],//data for each lightray
 	tau                 = 2*Math.PI,//used for conversion and polar coordinates
+	dTheta              = tau/400,
 	svg                 = d3.select("#simulation").append("svg")// add the svg element to body
 							.attr("width" , svgWidth)
 							.attr("height", svgHeight)// send down a light ray on click
-							.on  ("click" , addRay);
+							.on  ("click" , function() 
+								{
+									var m = d3.mouse(this);// gets current mouse locvation on the svg
+									origin = {x:m[0],y:m[1]}
+									addRay(origin,tau/4,10,rayColor,true)
+								});
 
 
 var lineFunction = d3.svg.line()
     .x(function(d) { return d.x; })
     .y(function(d) { return d.y; })
-    .interpolate("none");
+    .interpolate("basis");
 
 svg.append("circle")
 	.attr("cx", svgWidth/2)
@@ -45,44 +52,46 @@ svg.append("circle")
 
 
 // this creates the cloud
-	// add desired number of particles to cloud array
-	for (var i = 0; i < numCloudParticles; i++) 
-	{
-		x = Math.random()*svgWidth/3+svgWidth/3
-		y = Math.random()*svgHeight/3+svgHeight/3
-		cloudParticles.push([x,y])
-	};
+// add desired number of particles to cloud array
+for (var i = 0; i < numCloudParticles; i++) 
+{
+	x = Math.random()*svgWidth/3+svgWidth/3
+	y = Math.random()*svgHeight/3+svgHeight/3
+	cloudParticles.push([x,y])
+};
 
-	// add a particle to the svg for each particle in the array
-	svg.selectAll("circle")
-		.data(cloudParticles)
-		.enter()
-		.append("circle")
-		.attr("cx",function(d){return(d[0])})
-		.attr("cy",function(d){return(d[1])})
-		.attr("r",cloudParticleRadius)
-		.attr("fill",cloudColor);// offwhite cloud
+// add a particle to the svg for each particle in the array
+svg.selectAll("circle")
+	.data(cloudParticles)
+	.enter()
+	.append("circle")
+	.attr("cx",function(d){return(d[0])})
+	.attr("cy",function(d){return(d[1])})
+	.attr("r",cloudParticleRadius)
+	.attr("fill",cloudColor);
 
-// add new light ray
-function addRay() {
 
-	// gets current mouse locvation on the svg
-	var m = d3.mouse(this);
+function addRay(origin,angle,r,color,refract)
+{
+	polCoords     = {r:r,theta:angle}
+	cartCoords    = polToCart(polCoords)
+	cartCoords.x += origin.x
+	cartCoords.y +=origin.y
 
-	// adds new ray info to data array
 	rayData.push(
 		{
 			lineData:
 			[
-				{x:m[0],y:m[1]},
-				{x:m[0],y:m[1]+10}
+				origin,
+				origin,
+				cartCoords
 			],
-			origin:
-			{x:m[0],y:m[1]},
-			class:'activeRay',
-			color:'rgba(255,255,255,.5)',
-			refract:true
+			origin  : origin,
+			class   :'activeRay',
+			color   : color,
+			refract : refract
 		})
+
 	svg.selectAll(".activeRay")
 		.data(rayData)// associate all lines with a value in the rayData array
 		.enter()// d3 magic
@@ -92,70 +101,38 @@ function addRay() {
 		.attr("class",function(d){return(d.class)})
 		.attr("stroke-width",rayWidth)
 		.attr("stroke",function(d){return(d.color)});
-}
+};
 
-function refract(origin,angle,increment,color)
+function refract(origin,theta,dTheta)
 {
-	polCoords = {r: cloudParticleRadius*2,theta:angle+increment}
-	cartCoords = polToCart(polCoords)
-	cartCoords.x+=origin.x
-	cartCoords.y+=origin.y
-
-	rayData.push({lineData:[{x:origin.x,y:origin.y},{x:cartCoords.x,y:cartCoords.y+.0001}],origin:{x:origin.x,y:origin.y},class:'activeRay',color:color,refract:false})
-	svg.selectAll(".activeRay")
-		.data(rayData)// associate all lines with a value in the rayData array
-		.enter()// d3 magic
-		.append("path")
-		.attr("fill", "none")
-		.attr("d", function(d){return(lineFunction(d.lineData))})
-		.attr("class",function(d){return(d.class)})
-		.attr("stroke-width",rayWidth)
-		.attr("stroke",rayColor);
+	addRay(origin , theta+dTheta   , cloudParticleRadius*2 ,"rgba(255,0,0,.2)"   , false)
+	addRay(origin , theta+2*dTheta , cloudParticleRadius*2 ,"rgba(255,127,0,.2)" , false)
+	addRay(origin , theta+3*dTheta , cloudParticleRadius*2 ,"rgba(255,255,0,.2)" , false)
+	addRay(origin , theta+4*dTheta , cloudParticleRadius*2 ,"rgba(0,255,0,.2)"   , false)
+	addRay(origin , theta+5*dTheta , cloudParticleRadius*2 ,"rgba(0,0,255,.2)"   , false)
+	addRay(origin , theta+6*dTheta , cloudParticleRadius*2 ,"rgba(143,0,255,.2)" , false)
 }
 
+function sun(frequency)
+{
+	randomInt = Math.random()
+	if (randomInt<=frequency)
+	{
+		x = Math.random()*svgWidth/3+svgWidth/3
+		y = Math.sqrt(Math.pow(500,2) -Math.pow(x-svgWidth/2,2))-450
+		var origin = {x:x,y:y}
+		addRay(origin,tau/4,.01,rayColor,true)
+	}
+}
 
-
-// draw updated line
 function replot()
 {
-	x = Math.floor(Math.random()*50)
-	if (x==4)
-	{
-
-			x = Math.random()*svgWidth/3+svgWidth/3
-			y = Math.sqrt(Math.pow(500,2) -Math.pow(x-svgWidth/2,2))-450
-			var m = [x,y]
-
-		// adds new ray info to data array
-		rayData.push(
-		{
-			lineData:
-			[
-				{x:m[0],y:m[1]},
-				{x:m[0],y:m[1]+10}
-			],
-			origin:
-			{x:m[0],y:m[1]},
-			class:'activeRay',
-			color:'rgba(255,255,255,.5)',
-			refract:true
-		})
-	svg.selectAll(".activeRay")
-		.data(rayData)// associate all lines with a value in the rayData array
-		.enter()// d3 magic
-		.append("path")
-		.attr("fill", "none")
-		.attr("d", function(d){return(lineFunction(d.lineData))})
-		.attr("class",function(d){return(d.class)})
-		.attr("stroke-width",rayWidth)
-		.attr("stroke",function(d){return(d.color)});
-
-	}
+	sun(.01)
 
 	// loop through all light rays
 	for (var i = rayData.length - 1; i >= 0; i--)
 	{
-		amnfdg = true
+		hasNotColided = true
 
 		var len  = rayData[i].lineData.length-1,
 			rayX = rayData[i].lineData[len].x,
@@ -169,39 +146,26 @@ function replot()
 				cloudParticleY = cloudParticles[j][1],
 				xSeperation    = Math.abs(rayX-cloudParticleX),
 				ySeperation    = Math.abs(rayY-cloudParticleY);
-
-			
-
 			if (xSeperation<cloudParticleRadius && ySeperation<cloudParticleRadius)
 			{
+				hasNotColided     = false
 				rayData[i].origin = {x:rayX,y:rayY}
-				center = rayData[i].origin
-
-				polCoords = {r:.1,theta:0}
-				polCoords.theta=Math.random()*tau
-				// polCoords.theta=.8
-				polCoords.r+=cloudParticleRadius*2.5
-				cartCoords = polToCart(polCoords)
-				cartCoords.x+=center.x
-				cartCoords.y+=center.y
-
+				center            = rayData[i].origin
+				polCoords         = {r:.1,theta:0}
+				polCoords.theta   = Math.random()*tau
+				polCoords.r      += cloudParticleRadius*2.5
+				cartCoords        = polToCart(polCoords)
+				cartCoords.x     += center.x
+				cartCoords.y     += center.y
 
 				rayData[i].lineData.push(cartCoords)
+
 				if (rayData[i].refract)
 				{
-					// rayData[i].lineData.splice(0,len-1)
-			  		// rayData[i].class = 'inactive'
+					rayData[i].color = 'rgba(0,0,0,0)'
+					refract(center,polCoords.theta,dTheta)
 			  		rayData[i].refract = false
-					refract(center,polCoords.theta,tau/500,"rgba(255,0,0,.2)")
-					refract(center,polCoords.theta,2*tau/500,"rgba(255,127,0,.2)")
-					refract(center,polCoords.theta,3*tau/500,"rgba(255,255,0,.2)")
-					refract(center,polCoords.theta,4*tau/500,"rgba(0,255,0,.2)")
-					refract(center,polCoords.theta,5*tau/500,"rgba(0,0,255,.2)")
-					refract(center,polCoords.theta,6*tau/500,"rgba(75,0,255,.2)")
 				}
-				amnfdg = false
-
-
 			}
 
 			// var len  = rayData[i].lineData.length-1;
@@ -209,46 +173,38 @@ function replot()
 			x = rayX-center.x
 			y = rayY-center.y
 			polCoords = cartToPol({x:x,y:y})
-			if (amnfdg)
-			{polCoords.r+=raySpeed;amnfdg = true}
-			
+			if (hasNotColided)
+			{
+				polCoords.r+=raySpeed;
+				hasNotColided = true
+			}
 			cartCoords = polToCart(polCoords)
 			cartCoords.x+=center.x
 			cartCoords.y+=center.y
 			rayData[i].lineData[len] = cartCoords
 		};
 
-
-
-		
-
 		// if the rayhits the bottom of the svg then leave it there as a little marker (color observation)
 		if (rayY>svgHeight || rayY<0 || rayX>svgWidth || rayX<0)
 		{
-		  rayData[i].lineData[len-1].y=svgHeight
-		  rayData[i].lineData[len].y=svgHeight
-
-		  rayData[i].lineData.splice(0,len-1)
-		  rayData[i].class = 'inactive'
-
-			
-
-		}
-
-		// change the class data so it doesnt keep being looped over after it hits the bottom
-		if (rayData[i].class == 'inactive')
-		{
-			rayData.splice(i,1);
+			collectLightRay(rayData[i],i)
+		  
 		}
 	}
 
 	// update svg with new data
 	svg.selectAll(".activeRay")
 		.attr("d", function(d){return(lineFunction(d.lineData))})
-		.attr("class",function(d){return(d.class)})
-		.attr("stroke",function(d){return(d.color)});//this actualy changes the class
+		.attr("stroke", function(d){return(d.color)})
+		.attr("class",function(d){return(d.class)});
 };
 
+function collectLightRay(ray,i)
+{
+	ray.lineData = [{x:0,y:0},{x:0,y:0}]
+	ray.class = 'inactive'
+	rayData.splice(i,1);
+}
 
 function polToCart(coordinates)
 {
@@ -280,21 +236,3 @@ var yScale = d3.scale.linear()
 
 
 window.setInterval(replot,iterTime)
-// try {
-// 	// Fix up for prefixing
-// 	window.AudioContext = window.AudioContext||window.webkitAudioContext;
-// 		context = new AudioContext();
-// 	}
-// 	catch(e) {
-// 		alert('Sorry, your browser doesnt support the "play" button :( \n try the latest firefox or chrome');
-// 	}
-// // ----- functions to listen to morse code -----
-// 	// function to play a tone
-// 	function note(start,length,freq){
-// 		oscillator = context.createOscillator();
-// 		oscillator.frequency.value = freq;
-// 		// oscillator.type = 4;
-// 		oscillator.connect(context.destination);
-// 		oscillator.start(start);
-// 		oscillator.stop(start+length)
-// 		}
